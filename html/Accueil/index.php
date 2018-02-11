6bfade357c2238811f7bba580567b33481db45cd
Roman Rieunier <rrieunier@ensc.fr>
2018-02-11 14:30:59 +0100

    PulpaColada - V0.0.4 Rajout de FontAwesome et d'un .htaccess

--- html/Accueil/index.php
+++ html/Accueil/index.php
@@ -7,10 +7,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Accueil</title>
+
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link href="/PulpaColada/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
+    <script src="/PulpaColada/js/bootstrap.min.js"></script>
 </head>
 <body>
-<h1>Inscrivez vous à notre évènement</h1>
-
-
+<div class="container">
+    <h1>Inscrivez vous à notre évènement</h1>
+</div>
 </body>
 </html>
