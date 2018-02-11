2ce105dd3b874cf56a0e11e78be89f91028f4a08
rimehaddad <rhaddad@ensc.fr>
2018-02-11 19:32:43 +0100

    PulpaColada - V0.0.7 IUBEfiuUBEAC

--- html/Accueil/index.php
+++ html/Accueil/index.php
@@ -15,7 +15,18 @@
             crossorigin="anonymous"></script>
     <script src="/PulpaColada/js/bootstrap.min.js"></script>
 </head>
+
+
+
+
+
+
+
+
+
+
 <body>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">PulpaColada</a>
@@ -42,6 +53,45 @@
         </div>
     </div>
 </nav>
+<style>
+    p {
+        text-align: center;
+        font-size: 60px;
+        margin-top:0px;
+    }
+</style>
+<!-- Oui bon je sais que c est pas le bon compte à rebours et que je suis pas douée mais j essaye-->
+<p id="demo"></p>
+<script>
+    // Set the date we're counting down to
+    var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        document.getElementById("demo").innerHTML = days + "j " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+
+        // If the count down is over, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+</script>
 <div class="container text-center">
 
     <section>
