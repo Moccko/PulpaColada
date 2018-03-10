d3ea1af13cc4e4b3d74a5942a7a617eff5649a4f
Roman Rieunier <rrieunier@ensc.fr>
2018-03-10 19:07:06 +0100

    PulpaColada - V0.0.42 MaJ script bdd

--- SQL/bdd.sql
+++ SQL/bdd.sql
@@ -2,20 +2,20 @@
 -- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Mar 10, 2018 at 04:18 PM
--- Server version: 5.6.38
--- PHP Version: 7.1.12
+-- Hôte : localhost:3306
+-- Généré le :  sam. 10 mars 2018 à 19:05
+-- Version du serveur :  5.6.38
+-- Version de PHP :  7.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `PulpaColada`
+-- Base de données :  `PulpaColada`
 --
 DROP DATABASE `PulpaColada`;
 
-CREATE DATABASE IF NOT EXISTS `PulpaColada`
+CREATE DATABASE `PulpaColada`
   DEFAULT CHARACTER SET utf8
   COLLATE utf8_general_ci;
 USE `PulpaColada`;
@@ -23,54 +23,38 @@ USE `PulpaColada`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ADHERENT`
---
-
-CREATE TABLE `ADHERENT` (
-  `id`          INT(11)     NOT NULL,
-  `prenom`      VARCHAR(20) NOT NULL,
-  `nom`         VARCHAR(50) NOT NULL,
-  `promo`       INT(11)     NOT NULL DEFAULT '0',
-  `commentaire` TEXT
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ADMIN`
+-- Structure de la table `ADMIN`
 --
 
 CREATE TABLE `ADMIN` (
   `id`          INT(11)      NOT NULL,
   `email`       VARCHAR(100) NOT NULL,
   `utilisateur` VARCHAR(20)  NOT NULL,
-  `mdp`         CHAR(128) DEFAULT NULL,
+  `mdp`         CHAR(128)             DEFAULT NULL,
   `prenom`      VARCHAR(20)  NOT NULL,
   `nom`         VARCHAR(50)  NOT NULL,
   `poste`       VARCHAR(30)  NOT NULL,
   `photo`       BLOB,
   `couverture`  BLOB,
-  `bio`         TEXT
+  `bio`         TEXT,
+  `lienFb`      VARCHAR(255)          DEFAULT NULL,
+  `active`      TINYINT(1)   NOT NULL DEFAULT '0'
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
 --
--- Dumping data for table `ADMIN`
+-- Déchargement des données de la table `ADMIN`
 --
 
-INSERT INTO `ADMIN` (`id`, `email`, `utilisateur`, `mdp`, `prenom`, `nom`, `poste`, `photo`, `couverture`, `bio`) VALUES
-  (45, 'rhaddad@ensc.fr', 'rhaddad', 'mdp', 'Rime', 'Haddad', 'Prez', NULL, NULL, NULL),
-  (47, 'rrieunier@ensc.fr', 'rrieunier',
-   '2fe353e5bdbf6b2a975fa995f65ed4c1b346a875adf495a92f4cd58a8cd7f7d17faabe62ece2f4ba3033de226ff93bd5c932ae4c27998ef8899eae0735996bb7',
-   'Roman', 'Rieunier', 'CM', NULL, NULL, NULL);
+INSERT INTO `ADMIN` (`id`, `email`, `utilisateur`, `mdp`, `prenom`, `nom`, `poste`, `photo`, `couverture`, `bio`, `lienFb`, `active`)
+VALUES
+  (45, 'rhaddad@ensc.fr', 'rhaddad', 'mdp', 'Rime', 'Haddad', 'Prez', NULL, NULL, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `AFFINITE`
+-- Structure de la table `AFFINITE`
 --
 
 CREATE TABLE `AFFINITE` (
@@ -83,7 +67,7 @@ CREATE TABLE `AFFINITE` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `EQUIPE`
+-- Structure de la table `EQUIPE`
 --
 
 CREATE TABLE `EQUIPE` (
@@ -97,17 +81,18 @@ CREATE TABLE `EQUIPE` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `EVENEMENT`
+-- Structure de la table `EVENEMENT`
 --
 
 CREATE TABLE `EVENEMENT` (
   `id`          INT(11)      NOT NULL,
   `nom`         VARCHAR(100) NOT NULL,
-  `debut`       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `fin`         TIMESTAMP    NULL     DEFAULT NULL,
-  `lieu`        VARCHAR(200)          DEFAULT NULL,
+  `date`        DATE         NOT NULL,
+  `debut`       TIME         NOT NULL,
+  `fin`         TIME         DEFAULT NULL,
+  `lieu`        VARCHAR(200) DEFAULT NULL,
   `description` TEXT         NOT NULL,
-  `lienFb`      VARCHAR(255)          DEFAULT NULL
+  `lienFb`      VARCHAR(255) DEFAULT NULL
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
@@ -115,7 +100,7 @@ CREATE TABLE `EVENEMENT` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `JOUEUR`
+-- Structure de la table `JOUEUR`
 --
 
 CREATE TABLE `JOUEUR` (
@@ -132,38 +117,33 @@ CREATE TABLE `JOUEUR` (
   DEFAULT CHARSET = utf8;
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `ADHERENT`
---
-ALTER TABLE `ADHERENT`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `ADMIN`
+-- Index pour la table `ADMIN`
 --
 ALTER TABLE `ADMIN`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `utilisateur` (`utilisateur`);
+  ADD UNIQUE KEY `utilisateur` (`utilisateur`),
+  ADD UNIQUE KEY `lienFb` (`lienFb`);
 
 --
--- Indexes for table `AFFINITE`
+-- Index pour la table `AFFINITE`
 --
 ALTER TABLE `AFFINITE`
   ADD PRIMARY KEY (`joueur`, `joueurVoulu`),
   ADD KEY `joueur2` (`joueurVoulu`);
 
 --
--- Indexes for table `EQUIPE`
+-- Index pour la table `EQUIPE`
 --
 ALTER TABLE `EQUIPE`
   ADD PRIMARY KEY (`numero`);
 
 --
--- Indexes for table `EVENEMENT`
+-- Index pour la table `EVENEMENT`
 --
 ALTER TABLE `EVENEMENT`
   ADD PRIMARY KEY (`id`),
@@ -171,7 +151,7 @@ ALTER TABLE `EVENEMENT`
   ADD UNIQUE KEY `lienFb` (`lienFb`);
 
 --
--- Indexes for table `JOUEUR`
+-- Index pour la table `JOUEUR`
 --
 ALTER TABLE `JOUEUR`
   ADD PRIMARY KEY (`id`),
@@ -181,53 +161,47 @@ ALTER TABLE `JOUEUR`
   ADD KEY `equipe` (`equipe`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `ADHERENT`
---
-ALTER TABLE `ADHERENT`
-  MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `ADMIN`
+-- AUTO_INCREMENT pour la table `ADMIN`
 --
 ALTER TABLE `ADMIN`
   MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 48;
+  AUTO_INCREMENT = 50;
 
 --
--- AUTO_INCREMENT for table `EQUIPE`
+-- AUTO_INCREMENT pour la table `EQUIPE`
 --
 ALTER TABLE `EQUIPE`
   MODIFY `numero` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `EVENEMENT`
+-- AUTO_INCREMENT pour la table `EVENEMENT`
 --
 ALTER TABLE `EVENEMENT`
   MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `JOUEUR`
+-- AUTO_INCREMENT pour la table `JOUEUR`
 --
 ALTER TABLE `JOUEUR`
   MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `AFFINITE`
+-- Contraintes pour la table `AFFINITE`
 --
 ALTER TABLE `AFFINITE`
   ADD CONSTRAINT `joueur1` FOREIGN KEY (`joueur`) REFERENCES `JOUEUR` (`id`),
   ADD CONSTRAINT `joueur2` FOREIGN KEY (`joueurVoulu`) REFERENCES `JOUEUR` (`id`);
 
 --
--- Constraints for table `JOUEUR`
+-- Contraintes pour la table `JOUEUR`
 --
 ALTER TABLE `JOUEUR`
   ADD CONSTRAINT `equipe` FOREIGN KEY (`equipe`) REFERENCES `EQUIPE` (`numero`);
